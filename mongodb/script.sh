9042bc0c6c4e626d6d4332ef7df97d138f01bb36
minhnguyennhatdev <boybetby@gmail.com>
2024-12-30 15:26:05 +0700

    update mongodb

--- mongodb/script.sh
+++ mongodb/script.sh
@@ -1,17 +1,20 @@
 echo "Starting setup MongoDB"
 
-docker-compose exec shard01-a sh -c "mongosh < /scripts/init-shard01.js"
-docker-compose exec shard02-a sh -c "mongosh < /scripts/init-shard02.js"
-docker-compose exec shard03-a sh -c "mongosh < /scripts/init-shard03.js"
+docker exec shard-01-node-a sh -c "mongosh < /scripts/init-shard01.js"
+docker exec shard-02-node-a sh -c "mongosh < /scripts/init-shard02.js"
+docker exec shard-03-node-a sh -c "mongosh < /scripts/init-shard03.js"
 
 sleep 1
 
-docker-compose exec configsvr01 sh -c "mongosh < /scripts/init-configserver.js"
+docker exec mongo-config-01 sh -c "mongosh < /scripts/init-configserver.js"
 
 sleep 2
 
-docker-compose exec router01 sh -c "mongosh < /scripts/init-router.js"
+docker exec router-01 sh -c "mongosh < /scripts/init-router.js"
 
-docker-compose exec router01 mongosh --port 27017
+docker exec router-01 mongosh --port 27017
 
 echo "MongoDB is ready to use"
+
+
+mongodb://mongodb://127.0.0.1:27117,127.0.0.1:27118/demo?retryWrites=true&w=majority&appName=demo
